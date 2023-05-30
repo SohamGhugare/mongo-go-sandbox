21b8dd5ab4ae2d10f9d9d761ab8753430114bc85
SohamGhugare <soham.ghugare@gmail.com>
2023-05-30 23:09:40 +0530

    feat: added client driver code

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Failed to load .env")
+	}
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		log.Fatal("MONGO_URI cannot be empty")
+	}
 }
